cb34b0af9eccf5534f3ac5bef80e7794779fa4d4
Danny Lin <danny@kdrag0n.dev>
2022-04-10 18:38:45 -0700

    Launcher3: Add support for enabling taskbar on phones
    - Use new API to enable/disable SysUI taskbar integration
    - User-facing setting to control taskbar eligibility override on phones
    - Reload by restarting (live reload would be much more complicated)
    - Update SysUI integration state before reloading
    
    Depends on frameworks/base commit:
        SystemUI: Add API for runtime taskbar config on phones
    
    Signed-off-by: DennySPb <dennyspb@gmail.com>
    Change-Id: Id1889ca9b102f94cc0a1161f49de88428dcc0943

--- src/com/android/launcher3/DeviceProfile.java
+++ src/com/android/launcher3/DeviceProfile.java
@@ -54,6 +54,7 @@ import java.util.List;
 public class DeviceProfile {
 
     public static final String KEY_ROW_HEIGHT = "pref_row_height";
+    public static final String KEY_PHONE_TASKBAR = "pref_allow_phone_taskbar";
 
     private static final int DEFAULT_DOT_SIZE = 100;
     // Ratio of empty space, qsb should take up to appear visually centered.
@@ -302,6 +303,8 @@ public class DeviceProfile {
             }
         }
 
+        boolean allowPhone = Utilities.getPrefs(context).getBoolean(KEY_PHONE_TASKBAR, false);
+        isTaskbarPresent = (isTablet || allowPhone) && ApiWrapper.TASKBAR_DRAWN_IN_PROCESS;
         if (isTaskbarPresent) {
             taskbarSize = res.getDimensionPixelSize(R.dimen.taskbar_size);
             stashedTaskbarSize = res.getDimensionPixelSize(R.dimen.taskbar_stashed_size);
--- src/com/android/launcher3/InvariantDeviceProfile.java
+++ src/com/android/launcher3/InvariantDeviceProfile.java
@@ -60,6 +60,7 @@ import com.android.launcher3.util.Themes;
 import com.android.launcher3.util.WindowBounds;
 import com.android.launcher3.util.window.WindowManagerProxy;
 
+import com.android.quickstep.SystemUiProxy;
 import org.xmlpull.v1.XmlPullParser;
 import org.xmlpull.v1.XmlPullParserException;
 
@@ -333,6 +334,14 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
             case KEY_ALLAPPS_THEMED_ICONS:
                 onConfigChanged(mContext);
                 break;
+            case DeviceProfile.KEY_PHONE_TASKBAR:
+                // Create the illusion of this taking effect immediately
+                // Also needed because TaskbarManager inits before SystemUiProxy on start
+                boolean enabled = Utilities.getPrefs(mContext).getBoolean(DeviceProfile.KEY_PHONE_TASKBAR, false);
+                SystemUiProxy.INSTANCE.get(mContext).setTaskbarEnabled(enabled);
+
+                onConfigChanged(mContext, true);
+                break;
         }
     }
 
@@ -471,6 +480,10 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
     }
 
     private void onConfigChanged(Context context) {
+        onConfigChanged(context, false);
+    }
+
+    private void onConfigChanged(Context context, boolean taskbarChanged) {
         Object[] oldState = toModelState();
 
         // Re-init grid
@@ -479,7 +492,7 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
 
         boolean modelPropsChanged = !Arrays.equals(oldState, toModelState());
         for (OnIDPChangeListener listener : mChangeListeners) {
-            listener.onIdpChanged(modelPropsChanged);
+            listener.onIdpChanged(modelPropsChanged, taskbarChanged);
         }
     }
 
@@ -746,7 +759,7 @@ public class InvariantDeviceProfile implements OnSharedPreferenceChangeListener 
         /**
          * Called when the device provide changes
          */
-        void onIdpChanged(boolean modelPropertiesChanged);
+        void onIdpChanged(boolean modelPropertiesChanged, boolean taskbarChanged);
     }
 
 
